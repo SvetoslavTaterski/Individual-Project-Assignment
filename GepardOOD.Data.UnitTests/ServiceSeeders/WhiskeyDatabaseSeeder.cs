3d422d9289a19e040698283c926ef08afa06212f
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-08-06 18:45:51 +0300

    Adding whiskeyService tests

--- GepardOOD.Data.UnitTests/ServiceSeeders/WhiskeyDatabaseSeeder.cs
+++ GepardOOD.Data.UnitTests/ServiceSeeders/WhiskeyDatabaseSeeder.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using GepardOOD.Data.Models;
+using GepardOOD.Web.Data;
 
 namespace GepardOOD.Data.UnitTests.ServiceSeeders
 {
-	internal class WhiskeyDatabaseSeeder
+	public static class WhiskeyDatabaseSeeder
 	{
+		public static void SeedDatabase(GepardOODDbContext dbContext)
+		{
+			Whiskey whiskey = new Whiskey
+			{
+				Id = 25,
+				Name = "Whiskey",
+				Manufacturer = "Manufacturer",
+				Description = "Very cool description for a very cool whiskey ASkdasd sakd sakd",
+				ImageUrl = "Random",
+				Price = 23,
+				IsActive = true,
+				WhiskeyCategoryId = 1,
+				AssociateId = Guid.Parse("48942044-CE1F-4743-9FEC-15C6808BB427"),
+				ClientId = Guid.Parse("61A22398-32D9-4ADB-EDAF-08DB7B0B2A29")
+			};
+
+			Whiskey whiskey2 = new Whiskey
+			{
+				Id = 26,
+				Name = "Whiskey2",
+				Manufacturer = "Manufacturer2",
+				Description = "Very cool description for a very cool whiskey ASkdasd sakd sakd",
+				ImageUrl = "Random",
+				Price = 23,
+				IsActive = true,
+				WhiskeyCategoryId = 1,
+				AssociateId = Guid.Parse("48942044-CE1F-4743-9FEC-15C6808BB427"),
+				ClientId = Guid.Parse("61A22398-32D9-4ADB-EDAF-08DB7B0B2A29")
+			};
+
+			Whiskey whiskey3 = new Whiskey
+			{
+				Id = 27,
+				Name = "Whiskey3",
+				Manufacturer = "Manufacturer3",
+				Description = "Very cool description for a very cool whiskey ASkdasd sakd sakd",
+				ImageUrl = "Random",
+				Price = 23,
+				IsActive = true,
+				WhiskeyCategoryId = 1,
+				AssociateId = Guid.Parse("48942044-CE1F-4743-9FEC-15C6808BB427"),
+				ClientId = Guid.Parse("61A22398-32D9-4ADB-EDAF-08DB7B0B2A29")
+			};
+
+			dbContext.Whiskeys.Add(whiskey);
+			dbContext.Whiskeys.Add(whiskey2);
+			dbContext.Whiskeys.Add(whiskey3);
+
+			dbContext.SaveChanges();
+		}
 	}
 }
---
+++ GepardOOD.Data.UnitTests/WhiskeyServiceTests.cs
@@ -0,0 +1,142 @@
+﻿using GepardOOD.Data.Models;
+using GepardOOD.Services.Data;
+using GepardOOD.Services.Data.Interfaces;
+using GepardOOD.Web.Data;
+using static GepardOOD.Data.UnitTests.ServiceSeeders.WhiskeyDatabaseSeeder;
+
+using Microsoft.EntityFrameworkCore;
+using GepardOOD.Web.ViewModels.Whiskey;
+
+namespace GepardOOD.Data.UnitTests
+{
+	[TestFixture]
+	public class WhiskeyServiceTests
+	{
+		private GepardOODDbContext dbContext;
+		private DbContextOptions<GepardOODDbContext> dbOptions;
+
+		private IWhiskeyService whiskeyService;
+
+		[OneTimeSetUp]
+		public void OneTimeSetUp()
+		{
+			dbOptions = new DbContextOptionsBuilder<GepardOODDbContext>()
+				.UseInMemoryDatabase(databaseName: "GepardOODDbContext")
+				.Options;
+
+			dbContext = new GepardOODDbContext(dbOptions);
+
+			this.dbContext.Database.EnsureCreated();
+
+			SeedDatabase(this.dbContext);
+
+			whiskeyService = new WhiskeyService(dbContext);
+		}
+
+		[Test]
+		[TestCase(25)]
+		public async Task IsWhiskeyExistingByIdAsync(int whiskeyId)
+		{
+			IWhiskeyService whiskeyService = new WhiskeyService(dbContext);
+
+			var serviceWhiskey = await whiskeyService.ExistsByIdAsync(whiskeyId);
+
+			var dbWhiskey = await dbContext.Whiskeys.AnyAsync(b => b.Id == whiskeyId);
+
+			Assert.AreEqual(serviceWhiskey, dbWhiskey);
+		}
+
+		[Test]
+		[TestCase(1)]
+		public async Task Test_DeleteWhiskeyByIdAsync(int id)
+		{
+			IWhiskeyService whiskeyService = new WhiskeyService(dbContext);
+
+			var serviceResult = whiskeyService.DeleteWhiskeyByIdAsync(id);
+
+			Assert.IsNotNull(serviceResult);
+
+		}
+
+		[Test]
+		[TestCase(26, "48942044-CE1F-4743-9FEC-15C6808BB427")]
+		public async Task Test_IsAssociateWithIdOwnerOfSodaWithIdAsync(int whiskeyId, string associateId)
+		{
+			IWhiskeyService whiskeyService = new WhiskeyService(dbContext);
+
+			Whiskey whiskey = dbContext.Whiskeys.Where(b => b.IsActive).First(b => b.Id == whiskeyId);
+
+			bool isAssociateIdSameService =
+				await whiskeyService.IsAssociateWithIdOwnerOfWhiskeyWithIdAsync(whiskeyId, associateId);
+
+			bool isAssociateIdSame = whiskey.AssociateId.ToString() == associateId;
+
+			Assert.AreEqual(isAssociateIdSame, isAssociateIdSameService);
+		}
+
+		[Test]
+		[TestCase(25)]
+		public async Task Test_GetDetailsByIdAsync(int whiskeyId)
+		{
+			IWhiskeyService whiskeyService = new WhiskeyService(dbContext);
+
+			var result = whiskeyService.GetDetailsByIdAsync(whiskeyId);
+
+			Assert.IsNotNull(result);
+
+			Assert.IsInstanceOf<Task<WhiskeyDetailsViewModel>>(result);
+		}
+
+		[Test]
+		[TestCase(25)]
+		public async Task Test_GetWhiskeyForEditByIdAsync(int whiskeyId)
+		{
+			IWhiskeyService whiskeyService = new WhiskeyService(dbContext);
+
+			var result = whiskeyService.GetWhiskeyForEditByIdAsync(whiskeyId);
+
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOf<Task<WhiskeyFormModel>>(result);
+		}
+
+		[Test]
+		[TestCase(25)]
+		public async Task Test_GetWhiskeyForDeleteByIdAsync(int whiskeyId)
+		{
+			IWhiskeyService whiskeyService = new WhiskeyService(dbContext);
+
+			var result = whiskeyService.GetWhiskeyForDeleteByIdAsync(whiskeyId);
+
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOf<Task<WhiskeyPreDeleteViewModel>>(result);
+		}
+
+		[Test]
+		[TestCase(25)]
+		public async Task Test_DeleteWhiskeyByIdAsyncIsWhiskeyPropertyChanged(int whiskeyId)
+		{
+			IWhiskeyService whiskeyService = new WhiskeyService(dbContext);
+
+			Whiskey whiskey = await dbContext
+				.Whiskeys
+				.Where(b => b.IsActive)
+				.FirstAsync(b => b.Id == whiskeyId);
+
+			whiskeyService.DeleteWhiskeyByIdAsync(whiskey.Id);
+
+			Assert.IsFalse(whiskey.IsActive);
+		}
+
+		[Test]
+		[TestCase("48942044-CE1F-4743-9FEC-15C6808BB427")]
+		public async Task Test_AllByAssociateIdAsync(string associateId)
+		{
+			IWhiskeyService WhiskeyService = new WhiskeyService(dbContext);
+
+			var result = WhiskeyService.AllByAssociateIdAsync(associateId);
+
+			Assert.IsNotNull(result);
+			Assert.IsInstanceOf<Task<IEnumerable<WhiskeyAllViewModel>>>(result);
+		}
+	}
+}
